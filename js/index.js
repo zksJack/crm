e7274652c4d9a6a04bfff6cb868c7613485c7589
zksJack <44696770+zksJack@users.noreply.github.com>
2020-08-28 22:11:46 +0800

    首界面初始

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-var inde  =1;
+$(function () {
+    init();
+    let $plan = $.Callbacks();
+    $plan.add((_, baseInfo) => {
+        $('.baseBox>span').html(`你好，${baseInfo.name || ''} `)
+    })
+    $plan.add(power => {
+        
+    })
+
+    async function init() {
+        let res = await axios.get('/user/login');
+        if (res.code != 0) {
+            alert('请先登录');
+            window.location.href = 'login.html';
+            return;
+        }
+        let [power, baseInfo] = await axios.all([
+            axios.get('/user/power'),
+            axios.get('/user/info')
+        ])
+        baseInfo.code == 0 ? baseInfo = baseInfo.data : null;
+        $plan.fire(power,baseInfo); 
+    }
+    $(".baseBox>a").click(async function(){
+        let res=await axios.get('/user/signout');
+        if(res.code ==0){
+            window.location.href='login.html';
+            return;
+        }
+        alert("网络不给力，稍后再试")
+    })
+})
